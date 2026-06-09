7a9d1ab458b20d98606bd9121224499c0f80b57e
iniminimi <iniminimi@users.noreply.github.com>
2026-06-09 20:33:56 +0200

    Move portal background from body to main, keep head opaque, clean up CSS

--- resources/views/components/layouts/public.blade.php
+++ resources/views/components/layouts/public.blade.php
@@ -38,9 +38,9 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
-<body class="wp-shell {{ $bodyClass ?? 'wp-public-body' }}{{ $portalBgUrl ? ' wp-has-portal-bg' : '' }}"
-      @if ($portalBgUrl) style="--wp-portal-bg: url('{{ $portalBgUrl }}');" @endif>
-    <main class="wp-portal">
+<body class="wp-shell {{ $bodyClass ?? 'wp-public-body' }}">
+    <main class="wp-portal"
+          @if ($portalBgUrl) style="--wp-portal-bg: url('{{ $portalBgUrl }}');" @endif>
         {{ $slot }}
         
         <div class="wp-portal-footer">
